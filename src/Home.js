a9316e4b63545d9831472be066513cbdf2a2355c
jingliu01 <60014535+jingliu01@users.noreply.github.com>
2020-01-27 11:00:49 -0500

    Update Home.js

--- src/Home.js
+++ src/Home.js
@@ -35,7 +35,7 @@ export default function Home() {
           </div>
           <div className="row">
             <span className="time">4 — 4:45 PM</span>
-            <span className="event">Technical Presentation</span>
+            <span className="event">Technical Presentation: Ceren Budak, Assistant Professor, School of Information, University of Michigan</span>
           </div>
           <div className="row">
             <span className="time">4:45 — 5 PM</span>
