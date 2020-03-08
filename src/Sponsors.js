910a6190cf6553950126826ade266a9affab1471
Rie Sadohara <56049845+RieSadohara@users.noreply.github.com>
2020-03-08 17:52:31 -0400

    Sponsor names were added.
    Copied the content from home/src/PosterGuidelines.js and edited it.

--- src/Sponsors.js
+++ src/Sponsors.js
@@ -2,15 +2,47 @@ import React from 'react';
 
 export default function Sponsors() {
   return (
-    <span
-      style={{
-        "fontSize": "1.5rem",
-        "fontFamily": "Roboto Condensed",
-        "margin": "0 0 5rem 0",
-        "lineHeight": "1.5",
-        "fontWeight": "800",
-      }}
-      aria-label="Page still under construction"
-      role="img">This page is still under construction 👷‍♀️</span>
+    <div className="section section-3">
+      <span className="subtitle">Sponsors</span>
+      <div className="call">
+        <h3>Gold ($2000 - $4500)</h3>
+          <ul>
+            <li>MSU College of Engineering</li>
+            <li>MSU Vice President of Research & Innovation</li>
+          </ul>
+       
+       <h3>Silver ($750 - $1500)</h3>
+          <ul>
+            <li>BMB</li>
+            <li>College of Natural Science</li>
+            <li><a target="_blank" rel="noopener noreferrer" href="https://ssda.msu.edu/">Social Science Data Analytics Initiative</a></li>
+          </ul>  
+
+       <h3>Bronze ($500 - $750)</h3>
+          <ul>
+            <li><a target="_blank" rel="noopener noreferrer" href="https://www.cstat.msu.edu/">Center for Statistical Training & Consulting - CSTAT</a></li>
+            <li><a target="_blank" rel="noopener noreferrer" href="http://digitalhumanities.msu.edu/">Digital Humanities</a></li>
+            <li><a target="_blank" rel="noopener noreferrer" href="https://broad.msu.edu/masters/business-analytics/">Business Analytics</a></li>
+            <li>CMSE</li>
+            <li>ICER</li>
+          </ul>   
+      </div>
+    </div>
+  
+#        <h3>Other Accommodations</h3>
+#        <p><b>Lactation Rooms</b><br />
+#        At the MSU Union, changing tables are available in all restroom. Additionally the the ladies restrooms on the first floor will have Lactation Rooms available. To access the Lactation Room, first visit the Welcome Desk on the First/Main Floor to retrieve the key.</p>
+ 
+#    <span
+#      style={{
+#        "fontSize": "1.5rem",
+#        "fontFamily": "Roboto Condensed",
+#        "margin": "0 0 5rem 0",
+#        "lineHeight": "1.5",
+#        "fontWeight": "800",
+#      }}
+#      aria-label="Page still under construction"
+#      role="img">This page is still under construction 👷‍♀️</span>
+
   )
 }
